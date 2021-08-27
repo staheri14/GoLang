98c6ddc4064ec61a11fef127232d644639e2fa2a
Sanaz <staheri14@ku.edu.tr>
2021-08-27 08:36:13 -0700

    adds description of deletenode

--- practices/HackerRanks/deleteNode.go
+++ practices/HackerRanks/deleteNode.go
@@ -1,5 +1,8 @@
 package main
 
+// Delete the node at a given position in a linked list and return a reference to the head node.
+// The head is at position 0. The list may be empty after you delete the node.
+// In that case, return a null value
 import (
 	"bufio"
 	"fmt"
